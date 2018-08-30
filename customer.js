df68b528346d74ff34d466323329fbf0bc92602e
laxmi1 <laxmi@techvoxinc.com>
2018-08-30 20:32:18 +0530

    added a way to handle invisible emlements

--- customer.js
+++ customer.js
@@ -4,14 +4,24 @@ var customer = function () {
     var newCustomerLink = element(by.css('[ui-sref="companies.new"]'));
     var name = element(by.xpath("//input[@placeholder='Name']"));
     var legalName = element(by.xpath("//input[@placeholder='Legal Name']"));
+
    
 
     this.create = function () {
         plusCreateButton.click();
         newCustomerLink.click();
         name.sendKeys("Customer "+Date.now());
-        legalName.sendKeys("TechvoxInc");
-        
+
+    	legalName.isDisplayed().then(function (isVisible) {
+    	if (isVisible) {
+        legalName.sendKeys("Legal Name ");
+    	} else {
+        // element is not visible
+        console.log('Legal Name is disabled in Customer Forms');
+    	}
+		});
+
+		
         browser.sleep(1000);
     };
 
