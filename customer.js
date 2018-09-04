851ef19cfc6f221a1c2995e3448ff86fd7304db4
Abhinav <abhinav@techvoxinc.com>
2018-09-04 10:14:00 +0530

    new customer

--- customer.js
+++ customer.js
@@ -4,8 +4,29 @@ var customer = function () {
     var newCustomerLink = element(by.css('[ui-sref="companies.new"]'));
     var name = element(by.xpath("//input[@placeholder='Name']"));
     var legalName = element(by.xpath("//input[@placeholder='Legal Name']"));
-
-   
+    var industry = element(by.xpath("//div[1][@class='col-sm-3']/vox-select-field/div/select"));
+    var primarycontact = element(by.xpath("//div[1][@class='col-sm-6']/vox-text-field/div/div/input"));
+    var primarycontactemail = element(by.xpath("//input[@placeholder='E-mail']"));
+    // var contactphone = element(by.xpath(".//*[@id='main-section']/div/div[2]/div/div/div[2]/div/form/div[1]/div/section[6]/div/div[3]/div[1]/div[1]/phone-field/div/div/div"));
+    var customeremail = element(by.xpath("//input[@placeholder='jsmith@acme.com']"));
+    // var customerphone = element(by.xpath("//div[1][@class='col-sm-9']/phone-field/div/div/div"));
+    var creditlimit = element(by.xpath("//input[@placeholder='Credit limit']"));
+    var discount = element(by.xpath("//input[@placeholder='Discount']"));
+    var vatnumber = element(by.xpath("//input[@placeholder='VAT Number']"));
+    var moreinfo = element(by.css('[ng-click="onClick()"]'));
+    var taxexemptcode = element(by.xpath("//input[@placeholder='Tax Exempt Code']"));
+    var accountnumber = element(by.xpath("//input[@placeholder='Account Number']"));
+    var facebook = element(by.xpath("//input[@placeholder='Facebook']"));
+    var twitter = element(by.xpath("//input[@placeholder='Twitter']"));
+    var website = element(by.xpath("//input[@placeholder='Website']"));
+    var lisencenumber = element(by.xpath("//input[@placeholder='License Number']"));
+    var tollfreenumber = element(by.xpath("//input[@placeholder='Toll Free Number']"));
+    var taxid = element(by.xpath("//input[@placeholder='Tax ID']"));
+    var localfile = element(by.xpath("//input[@placeholder='eg /Users/joe/Acme Inc. Folder']"));
+    var background = element(by.xpath("//textarea[@placeholder='Background Info']"));
+    var otherinfo = element(by.xpath("//textarea[@placeholder='Other Info']"));
+    var specialinfo = element(by.xpath("//div[@placeholder='Special Info']"));
+    var save = element(by.xpath("//button[@class='submit-button button']"));
 
     this.create = function () {
         plusCreateButton.click();
@@ -20,8 +41,174 @@ var customer = function () {
         console.log('Legal Name is disabled in Customer Forms');
     	}
 		});
+        
+        industry.click();
+        industry.options[industry.selectedIndex].value=4;
 
-		
+        primarycontact.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        primarycontact.sendKeys("Contact"+Date.now());    
+        } else {
+        console.log('Primary contact disabled in customer Forms');
+        }
+        });
+
+        primarycontactemail.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        primarycontactemail.sendKeys("abhinav@shopvox.com"); 
+        } else {
+        console.log('Primary Contact email disabled in customer Forms');    
+        }    
+        });
+          
+        // contactphone.isDisplayed().then(function (isVisible){
+        // if (isVisible) {
+        // contactphone.sendKeys("9999999999"); 
+        // } else {
+        // console.log('Contact phone disabled in customer Forms');    
+        // }    
+        // });
+
+        customeremail.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        customeremail.sendKeys("abhinav@techvoxinc.com"); 
+        } else {
+        console.log('Customermail disabled in customer Forms');    
+        }    
+        });
+        
+        // customerphone.isDisplayed().then(function (isVisible){
+        // if (isVisible) {
+        // customerphone.sendKeys("8888888888"); 
+        // } else {
+        // console.log('Customer phone disabled in customer Forms');    
+        // }    
+        // });
+        
+        creditlimit.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        creditlimit.sendKeys("1000"); 
+        } else {
+        console.log('Credit Limit disabled in customer Forms');    
+        }    
+        });
+        
+        discount.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        discount.sendKeys("10%"); 
+        } else {
+        console.log('Discount disabled in customer Forms');    
+        }    
+        });
+        
+        vatnumber.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        vatnumber.sendKeys("12345"); 
+        } else {
+        console.log('VAT Number disabled in customer Forms');    
+        }    
+        });
+
+        moreinfo.click();
+
+        taxexemptcode.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        taxexemptcode.sendKeys("4321"); 
+        } else {
+        console.log('Tax Exempt Code disabled in customer Forms');    
+        }    
+        });
+
+      
+        accountnumber.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        accountnumber.sendKeys("12345678"); 
+        } else {
+        console.log('Account Number disabled in customer Forms');    
+        }    
+        });
+        
+        facebook.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        facebook.sendKeys("www.facebook.com"); 
+        } else {
+        console.log('Facebook disabled in customer Forms');    
+        }    
+        });
+
+        twitter.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        twitter.sendKeys("www.twitter.com"); 
+        } else {
+        console.log('Twitter disabled in customer Forms');    
+        }    
+        });
+
+        website.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        website.sendKeys("www.google.com"); 
+        } else {
+        console.log('Website disabled in customer Forms');    
+        }    
+        });
+        
+        lisencenumber.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        lisencenumber.sendKeys("12345678"); 
+        } else {
+        console.log('License Number disabled in customer Forms');    
+        }    
+        });
+
+        tollfreenumber.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        tollfreenumber.sendKeys("9999999999"); 
+        } else {
+        console.log('Toll Free Number disabled in customer Forms');    
+        }    
+        });
+
+        taxid.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        taxid.sendKeys("GST-1234"); 
+        } else {
+        console.log('TaxID disabled in customer Forms');    
+        }    
+        });
+
+        localfile.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        localfile.sendKeys("/home/abhinav/Desktop/Images&PDF's/1022a38856696f3f5610a2eed0ce254e--earth-video-natural-materials.jpg"); 
+        } else {
+        console.log('TaxID disabled in customer Forms');    
+        }    
+        });
+
+        background.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        background.sendKeys("Test Background details of customer"); 
+        } else {
+        console.log('Background disabled in customer Forms');    
+        }    
+        });
+        
+        otherinfo.isDisplayed().then(function (isVisible){
+        if (isVisible){
+        otherinfo.sendKeys("Test Other info details");    
+        } else {
+        console.log('Other info disabled in customer Forms') 
+        }    
+        });
+
+        specialinfo.isDisplayed().then(function (isVisible){
+        if (isVisible) {
+        specialinfo.sendKeys("Test Special info details of customer"); 
+        } else {
+        console.log('Special info disabled in customer Forms');    
+        }    
+        });
+        save.click();
+        
         browser.sleep(1000);
     };
 
