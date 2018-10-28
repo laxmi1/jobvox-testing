c37b00a8a5f92310fae12c6f952bf5fc2768086f
bharath99 <bharath@techvoxinc.com>
2018-10-28 17:09:21 +0530

    Designed Reports for scripts

--- conf.js
+++ conf.js
@@ -1,19 +1,64 @@
 exports.config = {
-	
-	seleniumAddress: 'http://localhost:4444/wd/hub',
 
-	capabilities: {
-		'browserName': 'chrome',
-		'chromeOptions': {
-			'args': ['--no-sandbox', '--disable-infobars']
-		}
-	},
+    seleniumAddress: 'http://localhost:4444/wd/hub',
 
-	specs: ['maintest.js'],
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+          'args': ['--no-sandbox', '--disable-infobars']
+        }
+    },
 
-	jasmineNodeOpts: {
-		defaultTimeoutInterval: 5 * 30000,
-		showColors:true
-	}
+    specs: ['maintest.js'],
+
+    resultJsonOutputFile: './ReportInJson.json',
+
+    onPrepare: function() {
+
+        global.MAXWAITTIME = 20000;
+
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            savePath: './',
+            filePrefix: 'xmlresults'
+        }));
+
+
+        jasmine.getEnv().addReporter({
+            specDone: function(result) {
+                if (result.status == 'failed') {
+                    browser.getCapabilities().then(function(caps) {
+                        var browserName = caps.get('browserName');
+
+                    });
+                }
+            }
+        })
+    },
+
+    onComplete: function() {
+        var browserName, browserVersion;
+        var capsPromise = browser.getCapabilities();
+
+        capsPromise.then(function(caps) {
+            browserName = caps.get('browserName');
+            browserVersion = caps.get('version');
+
+            var HTMLReport = require('protractor-html-reporter');
+
+            testConfig = {
+                reportTitle: 'Test Execution Report',
+                outputPath: './JasmineRports',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,            };
+            new HTMLReport().from('xmlresults.xml', testConfig);
+        });
+    },
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 3000000
+    }
 
 };
